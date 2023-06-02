2a2c7113fd0ddd1cfa113569cc49405373cda618
alsrhkd77 <alsrhkd77@gmail.com>
2023-06-02 15:17:17 +0900

    LoadingIndicator  - 코드 재사용성, 디자인 통일, 유지보수 편의성 증대를 위한 LoadingIndicator
    Signed-off-by: alsrhkd77 <alsrhkd77@gmail.com>

---
+++ lib/widgets/loading_indicator.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingIndicator extends StatelessWidget {
+  final double size;
+
+  const LoadingIndicator({Key? key, this.size = 120.0}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: SpinKitFadingCube(
+          size: size,
+          color: Colors.blue,
+        ),
+      ),
+    );
+  }
+}
