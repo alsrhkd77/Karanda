a5eb005a3dbcc8a00befa0cb298d1e24b025b307
alsrhkd77 <alsrhkd77@gmail.com>
2024-03-04 00:19:54 +0900

    마레타 reconnect 수정
    Signed-off-by: alsrhkd77 <alsrhkd77@gmail.com>

--- lib/maretta/maretta_page.dart
+++ lib/maretta/maretta_page.dart
@@ -49,7 +49,7 @@ class _MarettaPageState extends State<MarettaPage> with WindowListener {
 
   @override
   void onWindowFocus() {
-    _provider.disconnect();
+    _provider.connect();
   }
 
   @override
