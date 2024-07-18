d290c5231e762e1a5faca5e4db3bb969e2637694
min <alsrhkd77@gmail.com>
2024-07-18 21:05:01 +0900

    router 분리
    Signed-off-by: min <alsrhkd77@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,50 +2,19 @@ import 'dart:io';
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/foundation.dart';
-import 'package:go_router/go_router.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:intl/intl.dart';
-import 'package:karanda/artifact/artifact_page.dart';
-import 'package:karanda/atoraxxion/yolunakea_moon_page.dart';
-import 'package:karanda/auth/auth_error_page.dart';
 import 'package:karanda/auth/auth_notifier.dart';
-import 'package:karanda/auth/auth_page.dart';
-import 'package:karanda/bdo_news/event_calendar/event_calendar_page.dart';
 import 'package:karanda/checklist/checklist_notifier.dart';
-import 'package:karanda/checklist/checklist_page.dart';
-import 'package:karanda/color_counter/color_counter_page.dart';
 import 'package:karanda/common/bdo_world_time_notifier.dart';
 import 'package:karanda/common/real_time_notifier.dart';
-import 'package:karanda/home/home_page.dart';
-import 'package:karanda/horse_status/horse_status_page.dart';
-import 'package:karanda/initializer/initializer_page.dart';
 import 'package:karanda/maretta/maretta_notifier.dart';
-import 'package:karanda/maretta/maretta_page.dart';
-import 'package:karanda/obs_widgets/obs_bdo_timer/obs_bdo_timer_page.dart';
-import 'package:karanda/obs_widgets/partrigio_page.dart';
-import 'package:karanda/settings/change_log_page.dart';
-import 'package:karanda/settings/karanda_info_page.dart';
+import 'package:karanda/route.dart';
 import 'package:karanda/settings/settings_notifier.dart';
-import 'package:karanda/settings/settings_page.dart';
-import 'package:karanda/settings/support_karanda_page.dart';
-import 'package:karanda/settings/theme_setting_page.dart';
-import 'package:karanda/ship_upgrading/ship_upgrading_page.dart';
-import 'package:karanda/trade/trade_calculator_page.dart';
-import 'package:karanda/trade_market/presets/cooking_box_page.dart';
-import 'package:karanda/trade_market/presets/dehkias_light_page.dart';
-import 'package:karanda/trade_market/presets/magical_lightstone_crystal_page.dart';
-import 'package:karanda/trade_market/presets/melody_of_stars_page.dart';
-import 'package:karanda/trade_market/trade_market_detail_page.dart';
 import 'package:karanda/trade_market/trade_market_notifier.dart';
-import 'package:karanda/trade_market/trade_market_page.dart';
 import 'package:window_manager/window_manager.dart';
-import 'atoraxxion/sycrakea_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import 'ship_extension/ship_extension_page.dart';
 import 'shutdown_scheduler/shutdown_scheduler_notifier.dart';
-import 'shutdown_scheduler/shutdown_scheduler_page.dart';
 //import 'package:flutter_web_plugins/url_strategy.dart' show usePathUrlStrategy;
 
 Future<void> main() async {
@@ -74,7 +43,7 @@ Future<void> main() async {
             Locale('ko', 'KR'),
           ],
           path: 'assets/translations',
-          fallbackLocale: const Locale('en', 'US'),
+          fallbackLocale: const Locale('ko', 'KR'),
           child: MyApp(),
         ),
       ));
@@ -96,174 +65,6 @@ const _floatingActionButtonTheme = FloatingActionButtonThemeData(
   backgroundColor: Colors.blue,
 );
 
-final GoRouter _router = GoRouter(
-  initialLocation: '/window-init',
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const HomePage(),
-      routes: [
-        GoRoute(
-            path: 'window-init',
-            builder: (context, state) => const InitializerPage(),
-            redirect: (BuildContext context, GoRouterState state) {
-              if (kIsWeb) {
-                return '/';
-              }
-              return null;
-            }),
-        GoRoute(
-          path: 'settings',
-          builder: (context, state) => const SettingsPage(),
-          routes: [
-            GoRoute(
-              path: 'auth/info',
-              builder: (context, state) => const AuthPage(
-                token: null,
-                refreshToken: null,
-              ),
-            ),
-            GoRoute(
-              path: 'auth/authenticate',
-              builder: (context, state) => AuthPage(
-                token: state.uri.queryParameters['token'],
-                refreshToken: state.uri.queryParameters['refresh-token'],
-              ),
-            ),
-            GoRoute(
-              path: 'theme',
-              builder: (context, state) => const ThemeSettingPage(),
-            ),
-            GoRoute(
-              path: 'support-karanda',
-              builder: (context, state) => const SupportKarandaPage(),
-            ),
-            GoRoute(
-              path: 'change-log',
-              builder: (context, state) => const ChangeLogPage(),
-            ),
-            GoRoute(
-              path: 'karanda-info',
-              builder: (context, state) => const KarandaInfoPage(),
-            ),
-          ],
-        ),
-        GoRoute(
-          path: 'auth/info',
-          builder: (context, state) => const AuthPage(
-            token: null,
-            refreshToken: null,
-          ),
-        ),
-        GoRoute(
-          path: 'auth/authenticate',
-          builder: (context, state) => AuthPage(
-            token: state.uri.queryParameters['token'],
-            refreshToken: state.uri.queryParameters['refresh-token'],
-          ),
-        ),
-        GoRoute(
-          path: 'auth/error',
-          builder: (context, state) => const AuthErrorPage(),
-        ),
-        GoRoute(
-          path: 'horse',
-          builder: (context, state) => const HorseStatusPage(),
-        ),
-        GoRoute(
-          path: 'event-calendar',
-          builder: (context, state) => const EventCalendarPage(),
-        ),
-        GoRoute(
-          path: 'sycrakea',
-          builder: (context, state) => const SycrakeaPage(),
-        ),
-        GoRoute(
-          path: 'yolunakea-moon',
-          builder: (context, state) => const YolunakeaMoonPage(),
-        ),
-        GoRoute(
-          path: 'shutdown-scheduler',
-          builder: (context, state) => const ShutdownSchedulerPage(),
-        ),
-        GoRoute(
-          path: 'artifact',
-          builder: (context, state) => const ArtifactPage(),
-        ),
-        GoRoute(
-          path: 'ship-extension',
-          builder: (context, state) => const ShipExtensionPage(),
-          redirect: (context, state) => '/ship-upgrading',
-        ),
-        GoRoute(
-          path: 'ship-upgrading',
-          builder: (context, state) => const ShipUpgradingPage(),
-        ),
-        GoRoute(
-          path: 'trade-calculator',
-          builder: (context, state) => const TradeCalculatorPage(),
-        ),
-        GoRoute(
-          path: 'auth',
-          builder: (context, state) => const AuthPage(
-            token: null,
-            refreshToken: null,
-          ),
-        ),
-        GoRoute(
-          path: 'checklist',
-          builder: (context, state) => const ChecklistPage(),
-        ),
-        GoRoute(
-          path: 'color-counter',
-          builder: (context, state) => const ColorCounterPage(),
-        ),
-        GoRoute(
-          path: 'maretta',
-          builder: (context, state) => const MarettaPage(),
-        ),
-        GoRoute(
-          path: 'trade-market',
-          builder: (context, state) => const TradeMarketPage(),
-          routes: [
-            GoRoute(
-              path: 'detail',
-              builder: (context, state) => TradeMarketDetailPage(
-                code: state.extra as String?,
-                name: state.uri.queryParameters['name'],
-              ),
-            ),
-            GoRoute(
-              path: 'cooking-box',
-              builder: (context, state) => const CookingBoxPage(),
-            ),
-            GoRoute(
-              path: 'melody-of-stars',
-              builder: (context, state) => const MelodyOfStarsPage(),
-            ),
-            GoRoute(
-              path: 'magical-lightstone-crystal',
-              builder: (context, state) => const MagicalLightstoneCrystalPage(),
-            ),
-            GoRoute(
-              path: 'dehkias-light',
-              builder: (context, state) => const DehkiasLightPage(),
-            ),
-          ],
-        ),
-        GoRoute(
-          path: 'broadcast-widget/bdo-timer',
-          builder: (context, state) => const ObsBdoTimerPage(),
-        ),
-        GoRoute(
-          path: 'broadcast-widget/partrigio',
-          builder: (context, state) => PartrigioPage(),
-        ),
-      ],
-    ),
-  ],
-);
-
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
   final GlobalKey<ScaffoldMessengerState> rootScaffoldMessengerKey =
@@ -280,7 +81,7 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(create: (_) => ShutdownSchedulerNotifier()),
         ChangeNotifierProvider(
-          create: (_) => AuthNotifier(rootScaffoldMessengerKey, _router),
+          create: (_) => AuthNotifier(rootScaffoldMessengerKey, router),
           lazy: false,
         ),
         ChangeNotifierProxyProvider<AuthNotifier, ChecklistNotifier>(
@@ -337,7 +138,7 @@ class MyApp extends StatelessWidget {
               //cardTheme: CardTheme(color: Color.fromRGBO(40, 40, 40, 1.0)),
             ),
             themeMode: settings.darkMode ? ThemeMode.dark : ThemeMode.light,
-            routerConfig: _router,
+            routerConfig: router,
           );
         },
       ),
---
+++ lib/route.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:karanda/artifact/artifact_page.dart';
+import 'package:karanda/atoraxxion/sycrakea_page.dart';
+import 'package:karanda/atoraxxion/yolunakea_moon_page.dart';
+import 'package:karanda/auth/auth_error_page.dart';
+import 'package:karanda/auth/auth_page.dart';
+import 'package:karanda/bdo_news/event_calendar/event_calendar_page.dart';
+import 'package:karanda/checklist/checklist_page.dart';
+import 'package:karanda/color_counter/color_counter_page.dart';
+import 'package:karanda/home/home_page.dart';
+import 'package:karanda/horse_status/horse_status_page.dart';
+import 'package:karanda/initializer/initializer_page.dart';
+import 'package:karanda/maretta/maretta_page.dart';
+import 'package:karanda/obs_widgets/obs_bdo_timer/obs_bdo_timer_page.dart';
+import 'package:karanda/obs_widgets/partrigio_page.dart';
+import 'package:karanda/settings/change_log_page.dart';
+import 'package:karanda/settings/karanda_info_page.dart';
+import 'package:karanda/settings/settings_page.dart';
+import 'package:karanda/settings/support_karanda_page.dart';
+import 'package:karanda/settings/theme_setting_page.dart';
+import 'package:karanda/ship_extension/ship_extension_page.dart';
+import 'package:karanda/ship_upgrading/ship_upgrading_page.dart';
+import 'package:karanda/shutdown_scheduler/shutdown_scheduler_page.dart';
+import 'package:karanda/trade/trade_calculator_page.dart';
+import 'package:karanda/trade_market/presets/cooking_box_page.dart';
+import 'package:karanda/trade_market/presets/dehkias_light_page.dart';
+import 'package:karanda/trade_market/presets/magical_lightstone_crystal_page.dart';
+import 'package:karanda/trade_market/presets/melody_of_stars_page.dart';
+import 'package:karanda/trade_market/trade_market_detail_page.dart';
+import 'package:karanda/trade_market/trade_market_page.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/window-init',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomePage(),
+      routes: [
+        GoRoute(
+            path: 'window-init',
+            builder: (context, state) => const InitializerPage(),
+            redirect: (BuildContext context, GoRouterState state) {
+              if (kIsWeb) {
+                return '/';
+              }
+              return null;
+            }),
+        GoRoute(
+          path: 'settings',
+          builder: (context, state) => const SettingsPage(),
+          routes: [
+            GoRoute(
+              path: 'auth/info',
+              builder: (context, state) => const AuthPage(
+                token: null,
+                refreshToken: null,
+              ),
+            ),
+            GoRoute(
+              path: 'auth/authenticate',
+              builder: (context, state) => AuthPage(
+                token: state.uri.queryParameters['token'],
+                refreshToken: state.uri.queryParameters['refresh-token'],
+              ),
+            ),
+            GoRoute(
+              path: 'theme',
+              builder: (context, state) => const ThemeSettingPage(),
+            ),
+            GoRoute(
+              path: 'support-karanda',
+              builder: (context, state) => const SupportKarandaPage(),
+            ),
+            GoRoute(
+              path: 'change-log',
+              builder: (context, state) => const ChangeLogPage(),
+            ),
+            GoRoute(
+              path: 'karanda-info',
+              builder: (context, state) => const KarandaInfoPage(),
+            ),
+          ],
+        ),
+        GoRoute(
+          path: 'auth/info',
+          builder: (context, state) => const AuthPage(
+            token: null,
+            refreshToken: null,
+          ),
+        ),
+        GoRoute(
+          path: 'auth/authenticate',
+          builder: (context, state) => AuthPage(
+            token: state.uri.queryParameters['token'],
+            refreshToken: state.uri.queryParameters['refresh-token'],
+          ),
+        ),
+        GoRoute(
+          path: 'auth/error',
+          builder: (context, state) => const AuthErrorPage(),
+        ),
+        GoRoute(
+          path: 'horse',
+          builder: (context, state) => const HorseStatusPage(),
+        ),
+        GoRoute(
+          path: 'event-calendar',
+          builder: (context, state) => const EventCalendarPage(),
+        ),
+        GoRoute(
+          path: 'sycrakea',
+          builder: (context, state) => const SycrakeaPage(),
+        ),
+        GoRoute(
+          path: 'yolunakea-moon',
+          builder: (context, state) => const YolunakeaMoonPage(),
+        ),
+        GoRoute(
+          path: 'shutdown-scheduler',
+          builder: (context, state) => const ShutdownSchedulerPage(),
+        ),
+        GoRoute(
+          path: 'artifact',
+          builder: (context, state) => const ArtifactPage(),
+        ),
+        GoRoute(
+          path: 'ship-extension',
+          builder: (context, state) => const ShipExtensionPage(),
+          redirect: (context, state) => '/ship-upgrading',
+        ),
+        GoRoute(
+          path: 'ship-upgrading',
+          builder: (context, state) => const ShipUpgradingPage(),
+        ),
+        GoRoute(
+          path: 'trade-calculator',
+          builder: (context, state) => const TradeCalculatorPage(),
+        ),
+        GoRoute(
+          path: 'auth',
+          builder: (context, state) => const AuthPage(
+            token: null,
+            refreshToken: null,
+          ),
+        ),
+        GoRoute(
+          path: 'checklist',
+          builder: (context, state) => const ChecklistPage(),
+        ),
+        GoRoute(
+          path: 'color-counter',
+          builder: (context, state) => const ColorCounterPage(),
+        ),
+        GoRoute(
+          path: 'maretta',
+          builder: (context, state) => const MarettaPage(),
+        ),
+        GoRoute(
+          path: 'trade-market',
+          builder: (context, state) => const TradeMarketPage(),
+          routes: [
+            GoRoute(
+              path: 'detail',
+              builder: (context, state) => TradeMarketDetailPage(
+                code: state.extra as String?,
+                name: state.uri.queryParameters['name'],
+              ),
+            ),
+            GoRoute(
+              path: 'cooking-box',
+              builder: (context, state) => const CookingBoxPage(),
+            ),
+            GoRoute(
+              path: 'melody-of-stars',
+              builder: (context, state) => const MelodyOfStarsPage(),
+            ),
+            GoRoute(
+              path: 'magical-lightstone-crystal',
+              builder: (context, state) => const MagicalLightstoneCrystalPage(),
+            ),
+            GoRoute(
+              path: 'dehkias-light',
+              builder: (context, state) => const DehkiasLightPage(),
+            ),
+          ],
+        ),
+        GoRoute(
+          path: 'broadcast-widget/bdo-timer',
+          builder: (context, state) => const ObsBdoTimerPage(),
+        ),
+        GoRoute(
+          path: 'broadcast-widget/partrigio',
+          builder: (context, state) => PartrigioPage(),
+        ),
+      ],
+    ),
+  ],
+);
