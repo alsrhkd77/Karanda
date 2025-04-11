f4f58bda2fd6bc447bb2d216465e042c0bae410b
min <alsrhkd77@gmail.com>
2025-04-12 02:44:17 +0900

    Android Platform 추가
    Signed-off-by: min <alsrhkd77@gmail.com>

---
+++ android/app/src/main/kotlin/KarandaStudio/Karanda/MainActivity.kt
@@ -0,0 +1,5 @@
+package KarandaStudio.Karanda
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/hwansangyeonhwa/karanda/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.hwansangyeonhwa.karanda
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
