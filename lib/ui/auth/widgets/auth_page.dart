38305c50b8bad1081cb193d62abefa4d6ffef8f0
min <alsrhkd77@gmail.com>
2025-04-16 01:09:05 +0900

    Discord 로고 이미지가 너무 커지지 않도록 수정
    Signed-off-by: min <alsrhkd77@gmail.com>

--- lib/ui/auth/widgets/auth_page.dart
+++ lib/ui/auth/widgets/auth_page.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:karanda/ui/auth/controllers/auth_controller.dart';
+import 'package:karanda/ui/core/theme/dimes.dart';
 import 'package:karanda/ui/core/ui/karanda_app_bar.dart';
 import 'package:karanda/ui/core/ui/page_base.dart';
 import 'package:provider/provider.dart';
@@ -15,18 +16,30 @@ class AuthPage extends StatelessWidget {
         icon: Icons.account_circle_outlined,
         title: context.tr("auth.social login"),
       ),
-      body: PageBase(children: [Card(
-        clipBehavior: Clip.antiAlias,
-        margin: const EdgeInsets.all(24.0),
-        child: InkWell(
-          onTap: AuthController(authService: context.read()).authentication,
-          child: Image.asset(
-            "assets/image/discord_full.png",
-            isAntiAlias: true,
-            fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        padding: Dimens.pagePadding,
+        child: Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: Dimens.pageMaxWidth),
+            child: Column(
+              children: [
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  child: InkWell(
+                    onTap: AuthController(authService: context.read())
+                        .authentication,
+                    child: Image.asset(
+                      "assets/image/discord_full.png",
+                      isAntiAlias: true,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
-      ),]),
+      ),
     );
   }
 }
