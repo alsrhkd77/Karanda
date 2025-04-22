3a9e14a798bf78876209daf3b6381ea4db4334ad
min <alsrhkd77@gmail.com>
2025-04-23 00:38:48 +0900

    오버레이 창 생성 시 topmost 설정
    Signed-off-by: min <alsrhkd77@gmail.com>

--- lib/utils/overlay_window_utils/overlay_window_utils_windows.dart
+++ lib/utils/overlay_window_utils/overlay_window_utils_windows.dart
@@ -29,7 +29,7 @@ abstract class OverlayWindowUtilsPlatform {
     SetWindowLongPtr(hWnd, GWL_STYLE, WS_POPUP);
     SetWindowPos(hWnd, NULL, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
     SetWindowLongPtr(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW | WS_EX_TOPMOST);
-    //SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, width.ceil(), height.ceil(), SWP_FRAMECHANGED | SWP_SHOWWINDOW);
+    SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_FRAMECHANGED | SWP_SHOWWINDOW);
     ShowWindow(hWnd, SW_MAXIMIZE);
   }
 
