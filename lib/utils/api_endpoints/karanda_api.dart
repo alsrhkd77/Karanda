20e50939974b3c94dc7e92c1663e3b843cc6e618
min <alsrhkd77@gmail.com>
2025-04-24 22:34:14 +0900

    서버 엔드포인트 목록 추가
    Signed-off-by: min <alsrhkd77@gmail.com>

--- lib/utils/api_endpoints/karanda_api.dart
+++ lib/utils/api_endpoints/karanda_api.dart
@@ -17,9 +17,27 @@ abstract final class KarandaApi {
 
   static const String itemImage = "$storage/bdo/item/image";
 
-  static const String authorization = "/auth/discord/authorization";
-  static const String tokenRefresh = "/auth/discord/refresh";
-  static const String unregister = "/auth/discord/unregister";
+  static const String _auth = "/auth";
+  static const String authorization = "$_auth/discord/authorization";
+  static const String tokenRefresh = "$_auth/discord/refresh";
+  static const String unregister = "$_auth/discord/unregister";
   static const String families = "/bdo-family/families";
-  static const String tradeMarketWaitList = "/trade-market/wait-list";
+  static const String _tradeMarket = "/trade-market";
+  static const String marketWaitList = "$_tradeMarket/wait-list";
+  static const String marketPriceDetail = "$_tradeMarket/detail";
+  static const String marketLatestPrice = "$_tradeMarket/latest";
+  static const String adventurerHub = "/adventurer-hub";
+  static const String createPost = "$adventurerHub/post/create";
+  static const String updatePost = "$adventurerHub/post/update";
+  static const String getRecentPosts = "$adventurerHub/posts";
+  static const String getPostDetail = "$adventurerHub/post/detail";
+  static const String openPost = "$adventurerHub/post/open";
+  static const String closePost = "$adventurerHub/post/close";
+  static const String joinToPost = "$adventurerHub/post/join";
+  static const String cancelToPost = "$adventurerHub/post/cancel";
+  static const String acceptApplicant = "$adventurerHub/post/accept";
+  static const String rejectApplicant = "$adventurerHub/post/reject";
+  static const String getApplicant = "$adventurerHub/post/applicant";
+  static const String getApplicants = "$adventurerHub/post/applicants";
+  static const String getUserJoined = "$adventurerHub/user/joined";
 }
