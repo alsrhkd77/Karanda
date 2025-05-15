0f30e47fa3ae7f1f12463243a9ed5a3fb0a36e59
min <alsrhkd77@gmail.com>
2025-05-15 17:27:15 +0900

    not-found 페이지 이미지 크기 문제 수정
    Signed-off-by: min <alsrhkd77@gmail.com>

--- lib/ui/core/ui/not_found_page.dart
+++ lib/ui/core/ui/not_found_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:karanda/ui/core/theme/dimes.dart';
 import 'package:karanda/ui/core/ui/karanda_app_bar.dart';
 
 class NotFoundPage extends StatelessWidget {
@@ -9,20 +10,34 @@ class NotFoundPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const KarandaAppBar(),
-      body: Center(
-        child: Column(
-          children: [
-            Image.asset("assets/image/exclamation.png"),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text("Page not found"),
-            ),
-            ElevatedButton.icon(
-              icon: const Icon(Icons.home),
-              onPressed: () => context.go("/"),
-              label: const Text("Home"),
-            )
-          ],
+      body: Padding(
+        padding: Dimens.pagePadding,
+        child: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: Image.asset(
+                  "assets/image/exclamation.png",
+                  fit: BoxFit.contain,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Page not found!",
+                  style: TextTheme.of(context).headlineMedium,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton.icon(
+                  icon: const Icon(Icons.home),
+                  onPressed: () => context.go("/"),
+                  label: const Text("Home"),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
